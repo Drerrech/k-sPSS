GPS polytope was produing small vectors, as well as there was a bug due to numerical erros, did a quick fix but should investigate

k_fail_GPS.py
@@ -3,7 +3,7 @@ import polytope_k_sPSS
 import torch
 
 def select_k_spss(predicted_k_fail, num_dim, delta):
-    return polytope_k_sPSS.default_k_sPSS(num_dim, predicted_k_fail) * delta
+    return polytope_k_sPSS.normalised_k_sPSS(num_dim, predicted_k_fail) * delta
 
 def get_random_rotation_matrix(n):
     i_hat = torch.randint(0, n-1, (1,)) # [0, n-1) (idx starts at 0)

polytope_k_sPSS.py
@@ -15,4 +15,35 @@ def default_k_sPSS(n, k):
 
     Z = null_space(V)
     
-    return torch.from_numpy(Z).float()
+    D = torch.from_numpy(Z).float()
+
+    # due to numerical instabilit will some times produce shape [X, >n]
+    # not sure about proper solution yet, will just crop it
+    D = D[:, :n]
+
+    return D
+
+def normalised_k_sPSS(n, k):
+    p = 2*k+n+1
+    d = p-n
+
+    V = np.zeros((d,p))
+
+    T = np.linspace(1,p,num=p)
+
+    for i in range(p):
+        V[:,i] = [T[i]**j for j in range(1,d+1)]
+
+    Z = null_space(V)
+    
+    D = torch.from_numpy(Z).float()
+
+    # due to numerical instabilit will some times produce shape [X, >n]
+    # not sure about proper solution yet, will just crop it
+    D = D[:, :n]
+
+    # normalise each vector
+    for i in range(D.shape[0]):
+        D[i] /= max(torch.linalg.vector_norm(D[i]), 1e-12)
+    
+    return D