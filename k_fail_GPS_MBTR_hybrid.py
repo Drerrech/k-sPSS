there was a bug in models, i was normalising which may have been causing the nans in the abnormality file... I will run the hyperparameter tuning overnight but for now i think what i should do is run it on the profiles and see if i get any abnormalities, i think i will, because some of the non-nan problems are just broken. like DANWOODLS

k_fail_GPS_MBTR_hybrid.py
@@ -5,7 +5,7 @@ import torch
 # basically MBTR but before building a model check for decrease in sampled and if so update like gps
 
 class MBTR_GPS_hybrid_k_fail:
-    def __init__(self, x, bb_k_fail_wrapper, delta, mu, eta, gamma, eps_stop, prediction_software, log_file_path, preferred_model_order=2, use_opportunistic_cpu_exploitation=True, opportunistic_cpu_exploitation_manual_point_limit=1e12): # f_omega will not be used as no problems in the test set have contraints
+    def __init__(self, x, bb_k_fail_wrapper, delta, mu, eta, gamma, eps_stop, prediction_software, log_file_path, preferred_model_order=2, use_opportunistic_cpu_exploitation=True, opportunistic_cpu_exploitation_manual_point_limit=1e12, use_orthogonal=True): # f_omega will not be used as no problems in the test set have contraints
         self.bb_k_fail_wrapper = bb_k_fail_wrapper
         self.x = x
         self.cur_f_val = self.bb_k_fail_wrapper.p_reuse.evaluate(self.x) # using hashing (or not at step 0) get the current value of f
@@ -21,6 +21,7 @@ class MBTR_GPS_hybrid_k_fail:
 
         self.use_opportunistic_cpu_exploitation = use_opportunistic_cpu_exploitation
         self.opportunistic_cpu_exploitation_manual_point_limit = opportunistic_cpu_exploitation_manual_point_limit
+        self.use_orthogonal = use_orthogonal
 
         self.log_file_path = log_file_path # path to .txt where info should be stored
         open(log_file_path, "w").close() # clear log file
@@ -62,7 +63,7 @@ class MBTR_GPS_hybrid_k_fail:
             # 1.1 - build points to build the model
             # TODO: test for oversupply - though get model and sltn methods might take care of this already?
             # print("P:", p)
-            points = self.x + self.delta*models.get_random_D_max_norm_1(num_cpus, self.n) # NOTE: without x_k for now, will add later (so we don't loose it)
+            points = self.x + self.delta*(models.get_orthogonal_Q_D_max_norm_q(num_cpus, self.n) if self.use_orthogonal else models.get_random_D_max_norm_1(num_cpus, self.n)) # NOTE: without x_k for now, will add later (so we don't loose it)
 
             # 1.2 get function value at points
             f_vals, completed, actual_batch_calls, actual_k = self.bb_k_fail_wrapper.batch_call(points)
@@ -144,7 +145,7 @@ class MBTR_GPS_hybrid_k_fail:
             # not included
         
         else: # exploitation
-            additional_points = self.x + self.delta*models.get_random_D_max_norm_1(num_cpus, self.n) # NOTE: without x_k for now, will add later (so we don't loose it)
+            additional_points = self.x + self.delta*(models.get_orthogonal_Q_D_max_norm_q(num_cpus, self.n) if self.use_orthogonal else models.get_random_D_max_norm_1(num_cpus, self.n)) # NOTE: without x_k for now, will add later (so we don't loose it)
             additional_f_vals, completed, actual_batch_calls, actual_k = self.bb_k_fail_wrapper.batch_call(additional_points)
             
             
@@ -209,7 +210,7 @@ class MBTR_GPS_hybrid_k_fail:
                 
                 # evaluate additional points: (num_cpus-(k+1)) new model points + (k+1) f_x_hat points so at least one makes it out
                 # if f_x_hat does not make it out due to underpredicted k, one full batch will be used up for f_x_hat
-                additional_points = self.x + self.delta*models.get_random_D_max_norm_1(num_cpus - (k_fail_predicted+1), self.n)
+                additional_points = self.x + self.delta*(models.get_orthogonal_Q_D_max_norm_q(num_cpus - (k_fail_predicted+1), self.n) if self.use_orthogonal else models.get_random_D_max_norm_1(num_cpus - (k_fail_predicted+1), self.n))
                 additional_points = torch.cat([additional_points, torch.ones((k_fail_predicted+1, self.n)) * x_hat]) # idxs [num_cpus-(k+1), num_cpus-1] are x_hat points
                 
                 additional_f_vals, completed, actual_batch_calls, actual_k = self.bb_k_fail_wrapper.batch_call(additional_points)

models.py
@@ -272,5 +272,58 @@ def get_random_D_max_norm_1(p, n):
         mask = norms < 1e-8
         vectors[mask] = (torch.rand(mask.sum(), n) - 0.5) * 2
         norms = torch.sqrt((vectors ** 2).sum(1))
-    vectors /= norms.max()          # ← divide whole batch by the single largest norm
+    vectors /= norms.max()          # divide whole batch by the single largest norm
     return vectors
+
+def random_householder_transform(A):
+    n = A.shape[1]
+
+    v = (torch.rand(n) - 0.5) * 2
+    while (v.norm() < 1e-8):
+        v = (torch.rand(n) - 0.5) * 2
+    v /= v.norm()
+
+    H = torch.eye(v.shape[0]) - 2 * torch.outer(v, v)
+    return A @ H
+
+def get_orthogonal_householder_D_max_norm_1(p, n):
+    double_eye = torch.stack([2*(-0.5 + i%2) * torch.eye(n)[i//2] for i in range(2*n)])
+    
+    num_double_eye = p // (2*n)
+    num_rest = p % (2*n)
+
+    D = []
+    for i in range(num_double_eye):
+        D.append(random_householder_transform(double_eye))
+    if num_rest != 0:
+        D.append(random_householder_transform(double_eye[:num_rest]))
+
+    D = torch.cat(D)
+
+    # for i in range(D.shape[0]):
+    #     D[i] *= max(1e-8, 1 - torch.rand(1)**6) # magic numbers 🤤
+
+    # norms = torch.sqrt((D ** 2).sum(1))
+    # D /= norms.max()
+
+    return D
+
+def get_orthogonal_Q_D_max_norm_q(p, n):
+    scale_factors = [1.0, 0.5, 2.0]
+    double_eye = torch.stack([2*(-0.5 + i%2) * torch.eye(n)[i//2] for i in range(2*n)])
+    
+    num_double_eye = p // (2*n)
+    num_rest = p % (2*n)
+
+    D = []
+    for i in range(num_double_eye):
+        Q, _ = torch.linalg.qr(torch.randn(n, n))
+        
+        D.append(scale_factors[i % len(scale_factors)] * double_eye @ Q)
+    if num_rest != 0:
+        Q, _ = torch.linalg.qr(torch.randn(n, n))
+        D.append(scale_factors[0] * double_eye[:num_rest] @ Q)
+
+    D = torch.cat(D)
+
+    return D